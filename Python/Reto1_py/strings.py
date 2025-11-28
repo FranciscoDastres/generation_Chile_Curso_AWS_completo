0dd9801d6bcbfa125374113b438c142df1dab5a4
franciscoDastres <franciscomeza.1234@gmail.com>
2025-11-28 10:02:10 -0300

    feat: :sparkles: adding new labs in repo  py

---
+++ Python/Reto1_py/strings.py
@@ -0,0 +1,6 @@
+print("python es la hostia")
+print("python", "es" "la hostia")
+print("python","es","brutal", sep = "-") ## separador para la string
+print("python","es","brutal", sep = "*") ## separador para la string
+print("Esto se imprime" , end =  "! ")
+print("en una linea")
